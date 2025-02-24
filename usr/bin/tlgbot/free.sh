59c350ccf9be2f5dba23c17e46f24a33b87d3bb3
Ivan Artemov <artemov_i@tii.ru>
2025-02-24 13:26:52 +0300

    fixed

--- usr/bin/tlgbot/free.sh
+++ usr/bin/tlgbot/free.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . /usr/share/libubox/jshn.sh
 json_init
-result_command="$(free | awk '/Mem/ {print "ОЗУ: всего "$2" Б,","использовано "$3" Б,","свободно "$4" Б"}')"
+result_command="$(free | awk '/Mem/ {print "ОЗУ: всего "$2" КБ,","использовано "$3" КБ,","свободно "$4" КБ"}')"
 json_add_string "message" "${result_command}"
 json_close_object
 echo "$(json_dump)"
